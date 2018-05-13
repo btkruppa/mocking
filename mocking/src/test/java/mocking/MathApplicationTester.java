9a7734881d600bab5f823c14023d5f3e10436efe
Blake Kruppa <blake.kruppa@wallawalla.edu>
2018-05-13 19:33:52 -0400

    Update MathApplicationTester.java

--- mocking/src/test/java/mocking/MathApplicationTester.java
+++ mocking/src/test/java/mocking/MathApplicationTester.java
@@ -1,5 +1,7 @@
 package mocking;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
@@ -25,6 +27,14 @@ public class MathApplicationTester {
 	public void testAdd() {
 		when(calcService.add(10.0, 20.0)).thenReturn(30.0);
 		Assert.assertEquals(30.00, mathApp.add(10.0, 20.0), 0.000000001);
+		verify(calcService, times(1)).add(10.0, 20.0);	
+	}
+	
+	@Test
+	public void serviceAddCalled() {
+		when(calcService.add(0.0, 0.0)).thenReturn(0.0);
+		mathApp.add(0.0, 0.0);
+		verify(calcService, times(1)).add(0.0, 0.0);	
 	}
 
 }
